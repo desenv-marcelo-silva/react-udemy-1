5b1acd84b3978991d89975dd5b43affe2d0963f0
Marcelo Lopes da Silva <desenv.marcelo.silva@gmail.com>
2018-11-18 23:06:35 -0200

    Componente início.
    Criação do terceiro componente. About.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React, { Component } from 'react';
 import Site from './site';
 import Rodape from './componentes/rodape';
 import Cabecalho from './componentes/cabecalho';
+import Inicio from './componentes/inicio';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Cabecalho />
+        <Inicio />
         <Site />
         <Rodape />
       </div>
