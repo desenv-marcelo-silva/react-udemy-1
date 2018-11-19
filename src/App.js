6a258ea99aa81dd61468693db6748fe657baeb4a
Marcelo Lopes da Silva <desenv.marcelo.silva@gmail.com>
2018-11-19 00:06:45 -0200

    Ajuste no início e separação do serviço.
    Faz ajuste no inicio, faltava uma seção.
    Faz a separação do serviço.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Site from './site';
 import Rodape from './componentes/rodape';
 import Cabecalho from './componentes/cabecalho';
 import Inicio from './componentes/inicio';
+import Servicos from './componentes/servicos';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
       <div className="App">
         <Cabecalho />
         <Inicio />
+        <Servicos />
         <Site />
         <Rodape />
       </div>
