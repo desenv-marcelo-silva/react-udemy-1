fe2a7d2579e8c5b1213886eaaad32eec55e2182c
Marcelo Lopes da Silva <desenv.marcelo.silva@gmail.com>
2018-11-18 22:31:06 -0200

    Componente cabeçalho.
    Criação do segundo componente. Cabeçalho.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+
 import Site from './site';
 import Rodape from './componentes/rodape';
+import Cabecalho from './componentes/cabecalho';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Cabecalho />
         <Site />
         <Rodape />
       </div>
