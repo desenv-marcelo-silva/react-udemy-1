353dd4f06fced0b60f754c58155e4169112e8229
Marcelo Lopes da Silva <desenv.marcelo.silva@gmail.com>
2018-11-20 01:32:46 -0200

    Adiciona o Firebase
    Configura e adiciona o Firebase.

---
+++ src/config/firebase-config.js
@@ -0,0 +1,17 @@
+const rebase = require('re-base')
+const firebase = require('firebase')
+
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyAneNvHOf_YlwRu-b917zkrO3j6xr_Q09A",
+    authDomain: "portfolio-curso-udemy.firebaseapp.com",
+    databaseURL: "https://portfolio-curso-udemy.firebaseio.com",
+    projectId: "portfolio-curso-udemy",
+    storageBucket: "portfolio-curso-udemy.appspot.com",
+    messagingSenderId: "976083890772"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const config = rebase.createClass(app.database());
+
+export default config
