6ac2d0ff6a5c702cad7e3dc69c92022234e6f094
Marcelo Lopes da Silva <desenv.marcelo.silva@gmail.com>
2018-11-26 23:45:33 -0200

    Configuração do formulário para cadastrar títulos.

--- src/admin/portifolioadmin.js
+++ src/admin/portifolioadmin.js
@@ -1,15 +1,55 @@
 import React, { Component } from 'react'
 
-import config, {storage} from '../config/firebase-config'
+import config, { storage } from '../config/firebase-config'
 
 class PortfolioAdmin extends Component {
     constructor(props) {
         super(props)
+
+        this.gravarPortfolio = this.gravarPortfolio.bind(this);
     }
- 
+
+    gravarPortfolio(event) {
+        console.log('gravar portfolio.');
+        console.log(this.titulo.value);
+        console.log(this.descricao.value);
+        console.log(this.imagem.value);
+        event.preventDefault();
+    } 
+
     render() {
         return (
-            <h2>Portifólio - Área administrativa</h2>
+            <div style={ {'padding': '150px'} }>
+                <form onSubmit={this.gravarPortfolio}>
+                    <div className="form-group">
+                        <label htmlFor="titulo">Título</label>
+                        <input type="text" 
+                            className="form-control" 
+                            id="titulo" 
+                            placeholder="Título" 
+                            ref={(ref) => this.titulo = ref}
+                            />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="descricao">Descrição</label>
+                        <textarea className="form-control" 
+                            id="descricao" 
+                            rows="3"
+                            ref={(ref) => this.descricao = ref}></textarea>
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="uploadImagem">Imagem</label>
+                        <input type="file"
+                             className="form-control-file" 
+                             id="uploadImagem" 
+                             ref={(ref) => this.imagem = ref}
+                             />
+                    </div>                    
+                    <button type="submit" className="btn btn-primary">Salvar</button>
+                </form>
+            </div>
         )
     }
 }
